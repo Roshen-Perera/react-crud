32c3348151ff9be26f0a9383a79736c625dfa4ad
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2024-12-30 00:23:42 +0530

    Customer model created

---
+++ models/Customer.ts
@@ -0,0 +1,16 @@
+export class Customer {
+    id: number;
+    name: string;
+    email: string;
+    phone: string;
+    address: string;
+
+    constructor(id: number, name: string, email: string, phone: string, address: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+    }
+}
+
